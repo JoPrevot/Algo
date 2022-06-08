41cec710a0a3f5f63eaa650da079442d25014667
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-08 22:38:24 +0200

    Algo - Exercice 3

---
+++ src/Exercice_3.java
@@ -0,0 +1,11 @@
+public class Exercice_3 {
+    public static void main(String[] args) {
+
+        String nom="Prevot";
+        String prenom="Jordan";
+        byte age=29;
+
+        System.out.println("Vous vous appelez "+nom +" " +prenom +" et vous avez " +age +" ans.");
+
+    }
+}
