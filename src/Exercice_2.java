7771e15d746fa827eb941d2e845ac2da1b391bb5
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-08 22:29:14 +0200

    Algo - Exercice 2

---
+++ src/Exercice_2.java
@@ -0,0 +1,12 @@
+public class Exercice_2 {
+    public static void main(String[] args) {
+
+        float diameter=15F;
+
+        float rayon=(diameter/2);
+
+        double surface= (rayon*rayon*Math.PI);
+
+        System.out.println("La surface du cercle est de "+ surface);
+    }
+}
