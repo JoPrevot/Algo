a06c591a6e4a695fd55ac42af2f1e3708c067f0b
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-08 23:32:51 +0200

    Algo - Exercice 5

---
+++ src/Exercice_5.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+
+public class Exercice_5 {
+    public static void main(String[] args) {
+
+        byte ageUtilisateur = 29;
+
+        System.out.println("L'utilisateur a " +ageUtilisateur +" ans .");
+
+        int age = ageUtilisateur;
+
+        Calendar cal = Calendar.getInstance();
+
+        int date = cal.get(Calendar.YEAR);
+
+        int Annee = date - age;
+
+        System.out.println("L'utilisateur est ne en " +Annee);
+
+    }
+}
