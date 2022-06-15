77005bc7c133f4ea44ccf1374182c2236d041fee
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-15 14:40:40 +0200

    TP POO - Exercice_1 avec constructeur

--- src/POO/Exo_POO/Exo_1/Main.java
+++ src/POO/Exo_POO/Exo_1/Main.java
@@ -6,42 +6,86 @@ public class Main {
     public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
-    Personne personne = new Personne();
+
+//    Personne personne = new Personne();
+
+//        System.out.println("Veuillez saisir un prenom : ");
+//
+//        personne.firstName = input.nextLine();
+//
+//        System.out.println("Veuillez saisir un nom : ");
+//
+//        personne.lastName = input.nextLine();
+//
+//        System.out.println("Veuillez saisir votre pays : ");
+//
+//        personne.country = input.nextLine();
+//
+//        System.out.println("Etes vous marrie ?");
+//
+//        String marriedQuestion = input.nextLine();
+//
+//        if (marriedQuestion.equalsIgnoreCase("Oui"))
+//        {
+//            personne.married = true;
+//        }
+//        else
+//        {
+//            personne.married  = false;
+//        }
+//
+//        System.out.println("Combien d'enfants avez-vous ?");
+//
+//        personne.numberChildren = input.nextByte();
+//
+//        System.out.printf("Vous etes %s, vous venez de %s.\n",
+//                personne.nomComplet(),personne.country);
+//
+//        System.out.println("Vous "+(personne.married?"etes marie":"n'etes pas marie")
+//                +" et vous avez " +personne.numberChildren +" enfant.");
+
+        Personne personne = new Personne("","","",false, (byte) 0);
+
+        System.out.printf("Vous vous appelez %s %s. \n", personne.getFirstName(),personne.getLastName());
+        System.out.printf("Vous habitez en %s. \n",personne.getCountry());
+        System.out.println("Vous "+(personne.getMarried()?"etes marie":"n'etes pas marie"));
+        System.out.printf("Vous avez %d enfants.",personne.getNumberChildren());
+        System.out.println("");
 
         System.out.println("Veuillez saisir un prenom : ");
 
-        personne.firstName = input.nextLine();
+        personne.setFirstName(input.nextLine());
 
         System.out.println("Veuillez saisir un nom : ");
 
-        personne.lastName = input.nextLine();
+        personne.setLastName(input.nextLine());
 
         System.out.println("Veuillez saisir votre pays : ");
 
-        personne.country = input.nextLine();
+        personne.setCountry(input.nextLine());
 
-        System.out.println("Etes vous marrie ?");
+        System.out.println("Etes vous marie ?");
 
         String marriedQuestion = input.nextLine();
 
         if (marriedQuestion.equalsIgnoreCase("Oui"))
         {
-            personne.married = true;
+            personne.setMarried(true);
         }
         else
         {
-            personne.married  = false;
+            personne.setMarried(false);
         }
 
         System.out.println("Combien d'enfants avez-vous ?");
 
-        personne.numberChildren = input.nextByte();
+        personne.setNumberChildren(input.nextByte());
 
-        System.out.printf("Vous etes %s, vous venez de %s.\n",
-                personne.nomComplet(),personne.country);
+        System.out.printf("Vous vous appelez %s %s. \n", personne.getFirstName(),personne.getLastName());
+        System.out.printf("Vous habitez en %s. \n",personne.getCountry());
+        System.out.println("Vous "+(personne.getMarried()?"etes marie":"n'etes pas marie"));
+        System.out.printf("Vous avez %d enfants.",personne.getNumberChildren());
 
-        System.out.println("Vous "+(personne.married?"etes marie":"n'etes pas marie")
-                +" et vous avez " +personne.numberChildren +" enfant.");
 
     }
 }
--- src/POO/Exo_POO/Exo_1/Personne.java
+++ src/POO/Exo_POO/Exo_1/Personne.java
@@ -2,15 +2,79 @@ package POO.Exo_POO.Exo_1;
 
 public class Personne {
 
-    String firstName;
-    String lastName;
-    String country;
-    Boolean married;
-    Byte numberChildren;
+//    String firstName;
+//    String lastName;
+//    String country;
+//    Boolean married;
+//    Byte numberChildren;
+//
+//    String nomComplet ()
+//    {
+//        return firstName + " " + lastName;
+//    }
 
+    private String firstName;
+    private String lastName;
+    private String country;
+    private Boolean married;
+    private Byte numberChildren;
+
+    public Personne (String firstName,String lastName, String country, Boolean married,Byte numberChildren)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.married = married;
+        this.numberChildren = numberChildren;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getLastName ()
+    {
+        return lastName;
+    }
+    public String getCountry ()
+    {
+        return country;
+    }
+    public Boolean getMarried ()
+    {
+        return married;
+    }
+    public Byte getNumberChildren ()
+    {
+        return numberChildren;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public void setMarried(Boolean married)
+    {
+        this.married = married;
+    }
+
+    public void setNumberChildren(Byte numberChildren)
+    {
+        this.numberChildren = numberChildren;
+    }
     String nomComplet ()
     {
         return firstName + " " + lastName;
     }
-
 }
