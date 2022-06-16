0c9032880e14ed17fa3be685ce2c84069c112389
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-16 16:43:54 +0200

    POO - Statique

---
+++ src/POO/MethodeAttriStatique/Validator.java
@@ -0,0 +1,12 @@
+package POO.MethodeAttriStatique;
+
+public class Validator{
+    public static boolean isEmail(String email){
+        return email.contains("@");
+    }
+
+    public static boolean minMax(String str,byte min,byte max){
+        return  str.length()>=min && str.length()<=max;
+    }
+
+}
